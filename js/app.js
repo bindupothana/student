12577cc019c58eee5993060c4f5ebf10cb341858
bindupothana <himabindu.pothana@navaratan.com>
2017-01-24 18:19:09 +0530

    Initial commit

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 var myApp = angular.module("myApp", []);
+
 myApp.controller("maincntrl", function ($scope) {
 $scope.submit_success = false;
 $scope.call = function () {
 $scope.server = angular.copy($scope.user);
 $scope.submit_success = true;
+
 }
 }
 );
---
+++ js/pattern/alph.js
@@ -0,0 +1,13 @@
+
+<!-- Script by hscripts.com -->
+<!-- More scripts @www.hscripts.com -->
+<script type="text/javascript">
+function AllowAlphabet(){
+if (!frm.alphabet.value.match(/^[a-zA-Z]+$/) && frm.alphabet.value !="")
+{
+frm.alphabet.value="";
+frm.alphabet.focus();
+alert("Please Enter only alphabets in text");
+}
+}
+</script><!-- Script by hscripts.com -->
